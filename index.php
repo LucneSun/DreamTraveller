51d75c2d4fd42b3600033f38a5f1a016d8153b05
LucneSun <joaolucasarasolaire@gmail.com>
2022-07-21 17:17:22 -0300

    feat: Added Login frontend(html/css) to project.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charset="UTF-8">
+
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+    </head>
+
+    <body>
+        <div class="login_form">
+            <form>
+                <label>Login<input type="text"/></label>
+                <label>Password<input type="password" placeholder="insira seu login"/></label>
+                <br>
+                <input type="submit" value="LOGIN"/>
+                <br>
+                <input type="button" value="RECUPERAR SENHA"/>
+            </form>
+        </div>
+    
+    </body>
+
+</html>
